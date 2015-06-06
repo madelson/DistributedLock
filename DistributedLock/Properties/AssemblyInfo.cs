aab6bc0883873a3addc46ebd940234bda7ea1644
Michael Adelson <mike.adelson314@gmail.com>
2015-06-06 16:14:25 -0400

    Added nuspec

--- DistributedLock/Properties/AssemblyInfo.cs
+++ DistributedLock/Properties/AssemblyInfo.cs
@@ -22,16 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6d4ab3bb-206b-46f6-9d66-6416a3432fba")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
