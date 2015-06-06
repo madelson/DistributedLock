e515e7893a1b2478a02c329da349f227aa7631ee
Michael Adelson <mike.adelson314@gmail.com>
2015-06-06 10:53:50 -0400

    Argument validation test for SystemLock

--- DistributedLock.Tests/SystemDistributedLockTest.cs
+++ DistributedLock.Tests/SystemDistributedLockTest.cs
@@ -11,6 +11,20 @@ namespace Medallion.Threading.Tests
     public class SystemDistributedLockTest : DistributedLockTestBase
     {
         [TestMethod]
+        public void TestBadConstructorArguments()
+        {
+            TestHelper.AssertThrows<ArgumentNullException>(() => this.CreateLock(null));
+            TestHelper.AssertThrows<ArgumentNullException>(() => this.CreateLock(""));
+            TestHelper.AssertThrows<FormatException>(() => this.CreateLock(new string('a', SystemDistributedLock.MaxLockNameLength + 1)));
+            TestHelper.AssertThrows<FormatException>(() => this.CreateLock(@"a\b"));
+
+            // weird but valid args
+            TestHelper.AssertDoesNotThrow(() => this.CreateLock(new string('a', SystemDistributedLock.MaxLockNameLength)).Acquire().Dispose());
+            TestHelper.AssertDoesNotThrow(() => this.CreateLock(" \t").Acquire().Dispose());
+            TestHelper.AssertDoesNotThrow(() => this.CreateLock("/a/b/c").Acquire().Dispose());
+        }
+
+        [TestMethod]
         public void TestGarbageCollection()
         {
             var @lock = this.CreateLock("gc_test");
