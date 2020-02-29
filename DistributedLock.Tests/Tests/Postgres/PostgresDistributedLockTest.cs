916a9be3469d51c98c296bd04d81b42144f8e739
Michael Adelson <mike.adelson314@gmail.com>
2020-02-29 06:37:05 -0500

    Add connection pruning test (currently just fails)

--- DistributedLock.Tests/Tests/Postgres/PostgresDistributedLockTest.cs
+++ DistributedLock.Tests/Tests/Postgres/PostgresDistributedLockTest.cs
@@ -30,6 +30,27 @@ namespace Medallion.Threading.Tests.Tests.Postgres
             Assert.IsNotNull(handle2);
         }
 
+        // todo probably just always needs keepalive
+        [Test]
+        public async Task TestIdleConnectionPruning()
+        {
+            var connectionString = new NpgsqlConnectionStringBuilder(TestingPostgresDistributedLockEngine.GetConnectionString())
+            {
+                ConnectionIdleLifetime = 1,
+                ConnectionPruningInterval = 1,
+                MaxPoolSize = 1,
+                Timeout = 2,
+            }.ConnectionString;
+
+            var @lock = new PostgresDistributedLock(new PostgresAdvisoryLockKey("IdeConPru"), connectionString);
+            using var handle1 = await @lock.AcquireAsync();
+            
+            await Task.Delay(TimeSpan.FromSeconds(.5));
+
+            using var handle2 = await @lock.TryAcquireAsync(TimeSpan.FromSeconds(5));
+            Assert.IsNotNull(handle2);
+        }
+
         // todo idle pruning interval?
     }
 }
