0dc7fae41ea002c03e6b52713c287ba7ada973f0
Michael Adelson <mike.adelson314@gmail.com>
2020-09-03 07:38:35 -0400

    Add test to verify ConfigureAwait(false) usage

--- DistributedLock.Tests/Tests/ApiTest.cs
+++ DistributedLock.Tests/Tests/ApiTest.cs
@@ -4,6 +4,9 @@ using System.Text;
 using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Medallion.Threading.Tests.Tests
 {
@@ -45,10 +48,30 @@ namespace Medallion.Threading.Tests.Tests
             }
         }
 
+        [TestCaseSource(nameof(DistributedLockAssemblies))]
+        public void TestLibrariesUseConfigureAwaitFalse(AssemblyName assemblyName)
+        {
+            var projectDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(CurrentFilePath()), "..", "..", assemblyName.Name));
+            var codeFiles = Directory.GetFiles(projectDirectory, "*.cs", SearchOption.AllDirectories);
+            Assert.IsNotEmpty(codeFiles);
+
+            var awaitRegex = new Regex(@"//.*|(?<await>\bawait\s)");
+            var configureAwaitRegex = new Regex(@"\.ConfigureAwait\(false\)|\.TryAwait\(\)");
+            foreach (var codeFile in codeFiles)
+            {
+                var code = File.ReadAllText(codeFile);
+                var awaitCount = awaitRegex.Matches(code).Cast<Match>().Count(m => m.Groups["await"].Success);
+                var configureAwaitCount = configureAwaitRegex.Matches(code).Count;
+                Assert.IsTrue(configureAwaitCount >= awaitCount, $"ConfigureAwait(false) count ({configureAwaitCount}) < await count ({awaitCount}) in {codeFile}");
+            }
+        }
+
         private static IEnumerable<Type> GetPublicTypes(Assembly assembly) => assembly.GetTypes()
 #if DEBUG
                 .Where(t => !(t.Namespace!.Contains(".Internal") && assembly.GetName().Name == "DistributedLock.Core"))
 #endif
                 .Where(t => t.IsPublic || t.IsNestedPublic);
+
+        private static string CurrentFilePath([CallerFilePath] string filePath = "") => filePath;
     }
 }
