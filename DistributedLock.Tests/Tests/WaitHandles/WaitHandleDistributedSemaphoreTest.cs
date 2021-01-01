466df500c7de79532253fe7014795892fa389fe8
Michael Adelson <mike.adelson314@gmail.com>
2020-12-31 21:50:19 -0500

    Fix test namespace

--- DistributedLock.Tests/Tests/WaitHandles/WaitHandleDistributedSemaphoreTest.cs
+++ DistributedLock.Tests/Tests/WaitHandles/WaitHandleDistributedSemaphoreTest.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Medallion.Threading.Tests.Tests.WaitHandles
+namespace Medallion.Threading.Tests.WaitHandles
 {
     [Category("CIWindows")]
     public class WaitHandleDistributedSemaphoreTest
