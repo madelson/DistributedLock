39dba9bd324416392f547c0303e66c7c25df0512
Sergey Komisarchik <skomisarchik@usetech.ru>
2021-02-23 20:54:10 +0300

    comments, tests

--- DistributedLock.Redis/Primitives/RedisScript.cs
+++ DistributedLock.Redis/Primitives/RedisScript.cs
@@ -21,9 +21,11 @@ namespace Medallion.Threading.Redis.Primitives
         }
 
         public RedisResult Execute(IDatabase database, TArgument argument, bool fireAndForget = false) =>
+            // database.ScriptEvaluate must be called instead of _script.Evaluate in order to respect the database's key prefix
             database.ScriptEvaluate(this._script, this._parameters(argument), flags: RedLockHelper.GetCommandFlags(fireAndForget));
 
         public Task<RedisResult> ExecuteAsync(IDatabaseAsync database, TArgument argument, bool fireAndForget = false) =>
+            // database.ScriptEvaluate must be called instead of _script.Evaluate in order to respect the database's key prefix
             database.ScriptEvaluateAsync(this._script, this._parameters(argument), flags: RedLockHelper.GetCommandFlags(fireAndForget));
 
         // send the smallest possible script to the server
