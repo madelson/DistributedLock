efa0aab4c610be129dffed9ff3cd8271e407e8c0
Michael Adelson <mike.adelson314@gmail.com>
2021-11-03 07:13:57 -0400

    Add infrastructure for testing against a local Oracle containerized instance

---
+++ DistributedLock.Tests/Infrastructure/Oracle/OracleSetUpFixture.cs
@@ -0,0 +1,99 @@
+﻿using Medallion.Shell;
+using NUnit.Framework;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medallion.Threading.Tests.Oracle
+{
+    /// <summary>
+    /// Starts and stops the oracle docker container if needed. For best performance, run "docker start oracle-distributed-lock" before running the
+    /// tests so that the container will just keep running.
+    /// 
+    /// Note: for this setup we're assuming that the Oracle DB has been set up to run in Docker.
+    /// 
+    /// Setup steps:
+    /// (1) Install Docker if needed
+    /// (2) Clone https://github.com/oracle/docker-images
+    /// (3) Follow instructions here: https://github.com/oracle/docker-images/tree/main/OracleDatabase/SingleInstance#oracle-database-container-images
+    ///     for running the buildContainerImage shell script. I ran "./buildContainerImage.sh -v 18.4.0 -x" because as of 2021-11-01 that was the version that
+    ///     came pre-baked with the repo
+    /// (4) Create the container with "docker run --name oracle-distributed-lock -p 1521:1521 oracle/database:18.4.0-xe", then stop it (e. g. from the docker desktop gui)
+    /// 
+    /// NOTE: After reading https://blog.simonpeterdebbarma.com/2020-04-memory-and-wsl/ I also added a %USERPROFILE%/.wslconfig containing the following:
+    /// [wsl2]
+    /// memory=1GB
+    /// </summary>
+    [SetUpFixture]
+    public class OracleSetUpFixture
+    {
+        private const string ContainerName = "oracle-distributed-lock";
+
+        private static readonly Shell.Shell Shell = new(o => o.ThrowOnError(true));
+
+        private bool _startedContainer;
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUpAsync()
+        {
+            // first, check if container has started
+            var containerLs = await Shell.Run("docker", new[] { "container", "ls" }).Task;
+            if (!containerLs.StandardOutput.Contains(ContainerName))
+            {
+                Console.WriteLine($"docker container ls:");
+                Console.WriteLine(containerLs);
+
+                Console.WriteLine("STARTING CONTAINER");
+
+                var stopwatch = Stopwatch.StartNew();
+                await Shell.Run("docker", new[] { "start", ContainerName }).Task;
+                this._startedContainer = true;
+                
+                Console.WriteLine($"CONTAINER STARTED ({stopwatch.Elapsed.TotalSeconds}s)");
+            }
+
+            var timeout = Task.Delay(TimeSpan.FromMinutes(5));
+            while (true)
+            {
+                var tryConnectTask = TryConnectAsync();
+                var completed = await Task.WhenAny(timeout, tryConnectTask);
+                if (completed == timeout) { throw new TimeoutException("Timed out trying to connect to Oracle"); }
+                if (await tryConnectTask) { return; }
+            }
+
+            async Task<bool> TryConnectAsync()
+            {
+                await using var connection = new OracleConnection(OracleCredentials.GetConnectionString(TestContext.CurrentContext.TestDirectory));
+                try
+                {
+                    await connection.OpenAsync();
+                    return true;
+                }
+                catch (OracleException ex) 
+                    when (
+                        ex.Number == 12541 // TNS: No listener
+                        || ex.Number == 12514 // TNS:listener does not currently know of service requested in connect descriptor
+                        || ex.Number == 12528 // TNS: listener: all appropriate instances are blocking new connections
+                    )
+                {
+                    return false;
+                }
+            }
+        }
+
+        [OneTimeTearDown]
+        public async Task OneTimeTearDown()
+        {
+            if (this._startedContainer)
+            {
+                Console.WriteLine("STOPPING CONTAINER");
+                var stopwatch = Stopwatch.StartNew();
+                await Shell.Run("docker", new[] { "stop", ContainerName }).Task;
+                Console.WriteLine($"CONTAINER STOPPED ({stopwatch.Elapsed.TotalSeconds}s)");
+            }
+        }
+    }
+}
---
+++ DistributedLock.Tests/Infrastructure/Shared/OracleCredentials.cs
@@ -0,0 +1,37 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medallion.Threading.Tests
+{
+    internal static class OracleCredentials
+    {
+        private static string GetPassword(string baseDirectory)
+        {
+            var file = Path.GetFullPath(Path.Combine(baseDirectory, "..", "..", "..", "credentials", "oracle.txt"));
+            if (!File.Exists(file)) { throw new InvalidOperationException($"Unable to find mysql credentials file {file}"); }
+            var lines = File.ReadAllLines(file);
+            if (lines.Length != 1) { throw new FormatException($"{file} must contain exactly 1 line of text"); }
+            return lines[0];
+        }
+
+        public static string GetConnectionString(string baseDirectory)
+        {
+            var password = GetPassword(baseDirectory);
+
+            return new OracleConnectionStringBuilder
+            {
+                DataSource = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))",
+                UserID = "SYSTEM",
+                Password = password,
+                PersistSecurityInfo = true,
+                // set a high pool size so that we don't empty the pool through things like lock abandonment tests
+                MaxPoolSize = 500,
+            }.ConnectionString;
+        }
+    }
+}
