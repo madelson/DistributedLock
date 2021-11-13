c9a347ad4633db9d389b81b7046bffd2ee41c249
Michael Adelson <mike.adelson314@gmail.com>
2021-11-12 20:40:32 -0500

    Fix #106 and #109

---
+++ DistributedLock.Tests/Tests/FileSystem/FileDistributedLockWindowsTest.cs
@@ -0,0 +1,37 @@
+﻿using Medallion.Threading.FileSystem;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Medallion.Threading.Tests.FileSystem
+{
+    [Category("CIWindows")]
+    public class FileDistributedLockWindowsTest
+    {
+        /// <summary>
+        /// Example of where always ignoring <see cref="UnauthorizedAccessException"/> during file creation
+        /// would be problematic.
+        /// </summary>
+        [Test]
+        public void TestThrowsUnauthorizedAccessExceptionInCaseOfFilePermissionViolation()
+        {
+            var @lock = new FileDistributedLock(new DirectoryInfo(@"C:\Windows"), Guid.NewGuid().ToString());
+            Assert.Throws<UnauthorizedAccessException>(() => @lock.TryAcquire()?.Dispose());
+        }
+
+        /// <summary>
+        /// Example of where always ignoring <see cref="UnauthorizedAccessException"/> during directory creation
+        /// would be problematic.
+        /// </summary>
+        [Test]
+        public void TestThrowsUnauthorizedAccessExceptionInCaseOfDirectoryPermissionViolation()
+        {
+            var @lock = new FileDistributedLock(new DirectoryInfo(@"C:\Windows\MedallionDistributedLock"), Guid.NewGuid().ToString());
+            var exception = Assert.Throws<InvalidOperationException>(() => @lock.TryAcquire()?.Dispose());
+            Assert.IsInstanceOf<UnauthorizedAccessException>(exception.InnerException);
+            Assert.IsFalse(Directory.Exists(Path.GetDirectoryName(@lock.Name)));
+        }
+    }
+}
