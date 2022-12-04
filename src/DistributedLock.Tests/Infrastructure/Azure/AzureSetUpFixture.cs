c2034b2b0f7b16aa535ea88f893c8d135d306042
Michael Adelson <mike.adelson314@gmail.com>
2022-12-04 08:45:27 -0500

    Fix AzureSetUpFixture cleanup logic

--- src/DistributedLock.Tests/Infrastructure/Azure/AzureSetUpFixture.cs
+++ src/DistributedLock.Tests/Infrastructure/Azure/AzureSetUpFixture.cs
@@ -8,12 +8,14 @@ namespace Medallion.Threading.Tests.Azure;
 [SetUpFixture]
 public class AzureSetUpFixture
 {
-    private Command? _azureStorageEmulatorCommand;
+    private const string EmulatorProcessName = "AzureStorageEmulator";
+
+    private bool _startedEmulator;
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        var existingProcesses = Process.GetProcessesByName("AzureStorageEmulator");
+        var existingProcesses = Process.GetProcessesByName(EmulatorProcessName);
         if (existingProcesses.Any())
         {
             Console.WriteLine($"Emulator already running (PID={existingProcesses[0].Id})");
@@ -21,17 +23,19 @@ public class AzureSetUpFixture
         }
         else
         {
-            var emulatorExePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Microsoft SDKs", "Azure", "Storage Emulator", "AzureStorageEmulator.exe");
+            var emulatorExePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Microsoft SDKs", "Azure", "Storage Emulator", $"{EmulatorProcessName}.exe");
             if (!File.Exists(emulatorExePath))
             {
-                throw new FileNotFoundException($"Could not locate the AzureStorageEmulator at {emulatorExePath}. This is required to run Azure tests. See https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator");
+                throw new FileNotFoundException($"Could not locate the {EmulatorProcessName} at {emulatorExePath}. This is required to run Azure tests. See https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator");
             }
 
+            // Note: we used to hang on to this command to kill it later; we no longer do that because this process seems to naturally exit
+            // by the end of the test and instead the emulator is running in another process. Therefore, we do a name-based lookup in teardown instead.
             var command = Command.Run(emulatorExePath, new[] { "start" }, o => o.StartInfo(i => i.RedirectStandardInput = false))
                 .RedirectTo(Console.Out)
                 .RedirectStandardErrorTo(Console.Error);
-            Console.WriteLine($"Launched AzureStorageEmulator (PID={command.ProcessId})");
-            this._azureStorageEmulatorCommand = command;
+            Console.WriteLine($"Launched {EmulatorProcessName}");
+            this._startedEmulator = true;
         }
 
         new BlobContainerClient(AzureCredentials.ConnectionString, AzureCredentials.DefaultBlobContainerName).CreateIfNotExists();
@@ -42,15 +46,13 @@ public class AzureSetUpFixture
     {
         new BlobContainerClient(AzureCredentials.ConnectionString, AzureCredentials.DefaultBlobContainerName).DeleteIfExists();
 
-        if (this._azureStorageEmulatorCommand != null)
+        if (this._startedEmulator)
         {
-            if (this._azureStorageEmulatorCommand.Task.IsCompleted)
+            foreach (var process in Process.GetProcessesByName(EmulatorProcessName))
             {
-                throw new InvalidOperationException($"AzureStorageEmulator exited unexpectedly with error code {this._azureStorageEmulatorCommand.Result.ExitCode}");
+                process.Kill();
+                process.WaitForExit();
             }
-
-            this._azureStorageEmulatorCommand.Kill();
-            this._azureStorageEmulatorCommand.Wait();
         }
     }
 }
