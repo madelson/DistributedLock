0094aa107d95daea10794dddafe30f4f10ab3037
Jihoon Park <bartleby2718@gmail.com>
2023-10-01 22:03:17 -0400

    Fix #162 RedisScript fails for the CultureInfo tr-TR (and use BacklogPolicy.FailFast for backward compatibility)

--- src/DistributedLock.Tests/Infrastructure/Redis/RedisServer.cs
+++ src/DistributedLock.Tests/Infrastructure/Redis/RedisServer.cs
@@ -35,7 +35,12 @@ internal class RedisServer
                 .RedirectStandardErrorTo(Console.Error);
             ActiveServersByPort.Add(this.Port, this);
         }
-        this.Multiplexer = ConnectionMultiplexer.Connect($"localhost:{this.Port}{(allowAdmin ? ",allowAdmin=true" : string.Empty)}");
+        this.Multiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions
+        {
+            EndPoints = { { "localhost", this.Port } },
+            AllowAdmin = allowAdmin,
+            BacklogPolicy = BacklogPolicy.FailFast, // for backward compatibility with pre-2.5 versions
+        });
         // Clean the db to ensure it is empty. Running an arbitrary command also ensures that 
         // the db successfully spun up before we proceed (Connect seemingly can complete before that happens). 
         // This is particularly important for cross-process locking where the lock taker process
--- src/DistributedLock.Tests/Tests/Redis/RedisDistributedLockTest.cs
+++ src/DistributedLock.Tests/Tests/Redis/RedisDistributedLockTest.cs
@@ -2,10 +2,12 @@
 using Moq;
 using NUnit.Framework;
 using StackExchange.Redis;
+using System.Globalization;
 
 namespace Medallion.Threading.Tests.Redis;
 
 [Category("CI")]
+[NonParallelizable] // one of the tests temporarily changes CurrentCulture
 public class RedisDistributedLockTest
 {
     [Test]
@@ -27,5 +29,35 @@ public class RedisDistributedLockTest
         Assert.Throws<ArgumentNullException>(() => new RedisDistributedLock("key", default(IEnumerable<IDatabase>)!));
         Assert.Throws<ArgumentNullException>(() => new RedisDistributedLock("key", new[] { database, null! }));
         Assert.Throws<ArgumentException>(() => new RedisDistributedLock("key", Enumerable.Empty<IDatabase>()));
+    }
+
+    /// <summary>
+    /// Reproduces the bug in https://github.com/madelson/DistributedLock/issues/162
+    /// where a Redis lock couldn't be acquired if the current CultureInfo was tr-TR,
+    /// due to a bug in the underlying StackExchange.Redis package.
+    /// 
+    /// This is because there are both "dotted i" and "dotless i" in some Turkic languages:
+    /// https://en.wikipedia.org/wiki/Dotted_and_dotless_I_in_computing
+    /// </summary>
+    [Test]
+    public void TestCanAcquireLockWhenCurrentCultureIsTurkishTurkey()
+    {
+        var originalCultureInfo = Thread.CurrentThread.CurrentCulture;
+
+        try
+        {
+            Thread.CurrentThread.CurrentCulture = new("tr-TR");
+            CultureInfo.CurrentCulture.ClearCachedData();
+
+            var @lock = new RedisDistributedLock(
+                TestHelper.UniqueName,
+                RedisServer.GetDefaultServer(0).Multiplexer.GetDatabase()
+            );
+            @lock.Acquire().Dispose();
+        }
+        finally
+        {
+            Thread.CurrentThread.CurrentCulture = originalCultureInfo;
+        }
     }
 }
