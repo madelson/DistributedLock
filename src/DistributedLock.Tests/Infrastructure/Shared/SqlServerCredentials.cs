75868f55163547eb15e4170ff4c5bf8c29a3032e
madelson <1269046+madelson@users.noreply.github.com>
2024-03-25 03:36:09 -0400

    Fix SqlServer test connection string

--- src/DistributedLock.Tests/Infrastructure/Shared/SqlServerCredentials.cs
+++ src/DistributedLock.Tests/Infrastructure/Shared/SqlServerCredentials.cs
@@ -4,7 +4,7 @@ internal static class SqlServerCredentials
 {
     public static readonly string ApplicationName = $"{typeof(SqlServerCredentials).Assembly.GetName().Name} ({TargetFramework.Current})";
 
-    public static readonly string ConnectionString = new System.Data.SqlClient.SqlConnectionStringBuilder
+    public static readonly string ConnectionString = new Microsoft.Data.SqlClient.SqlConnectionStringBuilder
         {
             DataSource = @"localhost", // localhost for SQL Developer, .\SQLEXPRESS for express
             InitialCatalog = "master",
@@ -12,7 +12,10 @@ internal static class SqlServerCredentials
             ApplicationName = ApplicationName,
             // set a high pool size so that we don't empty the pool through things like lock abandonment tests
             MaxPoolSize = 10000,
-            TrustServerCertificate = true,
+            // Allows us to connect to SQLExpress with Microsoft.Data.SqlClient while still being compatible
+            // with System.Data.SqlClient (alternative would be building the connection string with System.Data.SqlClient
+            // and doing TrustServerCertificate = true).
+            Encrypt = false,
         }
         .ConnectionString;
 }
