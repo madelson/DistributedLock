43200f26c1c82a7d94e574bef2c31a1d62138df7
thang <ducthangnguyen0609@gmail.com>
2025-08-24 21:54:51 +0700

    fix some test

--- src/DistributedLock.Tests/Tests/Etcd/EtcdSynchronizationProviderTest.cs
+++ src/DistributedLock.Tests/Tests/Etcd/EtcdSynchronizationProviderTest.cs
@@ -1,4 +1,5 @@
 ﻿using Medallion.Threading.Etcd;
+using Medallion.Threading.FileSystem;
 using NUnit.Framework;
 
 namespace Medallion.Threading.Tests.Etcd;
@@ -6,12 +7,20 @@ namespace Medallion.Threading.Tests.Etcd;
 public class EtcdSynchronizationProviderTest
 {
 
-    private readonly EtcdClusterSetup _etcdClusterSetup = new();
+    private readonly EtcdClusterSetup _etcdClusterBuilder = EtcdSetupFixture.EtcdClusterSetup;
+
+
+    [Test]
+    public void TestArgumentValidation()
+    {
+        Assert.Throws<ArgumentNullException>(() => new EtcdLeaseDistributedLockProvider(null!));
+    }
+
 
     [Test]
     public async Task BasicTest()
     {
-        var provider = new EtcdLeaseDistributedLockProvider(this._etcdClusterSetup.CreateClientToEtcdCluster());
+        var provider = new EtcdLeaseDistributedLockProvider(this._etcdClusterBuilder.CreateClientToEtcdCluster());
         var lock1 = provider.CreateLock("lockTest");
         await using var handle1 = await lock1.TryAcquireAsync();
         Assert.That(handle1, Is.Not.Null);
