563ece389cb0e4e6a47c194648620cab5ea3157d
thang.nguyen <thang.nguyen@orangelogic.com>
2025-08-24 00:01:26 +0000

    add stuff

---
+++ src/DistributedLock.Tests/Infrastructure/Shared/EtcdCredentials.cs
@@ -0,0 +1,7 @@
+
+namespace Medallion.Threading.Tests;
+
+public static class EtcdCredentials
+{
+    public const string EtcdHost = "http://localhost:2379?Secure=false?NoPassword=true";
+}
---
+++ src/DistributedLock.Tests/Tests/Etcd/EtcdSomeOtherTest.cs
@@ -0,0 +1,31 @@
+﻿using Medallion.Threading.FileSystem;
+using NUnit.Framework;
+
+namespace Medallion.Threading.Tests.Etcd;
+
+public class EtcdDistributedLockWindowsTest
+{
+    /// <summary>
+    /// Example of where always ignoring <see cref="UnauthorizedAccessException"/> during file creation
+    /// would be problematic.
+    /// </summary>
+    [Test]
+    public void TestThrowsUnauthorizedAccessExceptionInCaseOfFilePermissionViolation()
+    {
+        var @lock = new FileDistributedLock(new DirectoryInfo(@"C:\Windows"), Guid.NewGuid().ToString());
+        Assert.Throws<UnauthorizedAccessException>(() => @lock.TryAcquire()?.Dispose());
+    }
+
+    /// <summary>
+    /// Example of where always ignoring <see cref="UnauthorizedAccessException"/> during directory creation
+    /// would be problematic.
+    /// </summary>
+    [Test]
+    public void TestThrowsUnauthorizedAccessExceptionInCaseOfDirectoryPermissionViolation()
+    {
+        var @lock = new FileDistributedLock(new DirectoryInfo(@"C:\Windows\MedallionDistributedLock"), Guid.NewGuid().ToString());
+        var exception = Assert.Throws<InvalidOperationException>(() => @lock.TryAcquire()?.Dispose())!;
+        Assert.That(exception.InnerException, Is.InstanceOf<UnauthorizedAccessException>());
+        Assert.That(Directory.Exists(Path.GetDirectoryName(@lock.Name)), Is.False);
+    }
+}
