cd141257708fc80244263996f0ef27b83f2ce396
Joes <dygood@outlook.com>
2025-10-23 10:54:40 +0800

    style: fix code format

--- src/DistributedLock.MongoDB/MongoDistributedLock.IDistributedLock.cs
+++ src/DistributedLock.MongoDB/MongoDistributedLock.IDistributedLock.cs
@@ -6,28 +6,17 @@ public partial class MongoDistributedLock
 {
     // AUTO-GENERATED
 
-    IDistributedSynchronizationHandle? IDistributedLock.TryAcquire(TimeSpan timeout, CancellationToken cancellationToken)
-    {
-        return TryAcquire(timeout, cancellationToken);
-    }
-
-    IDistributedSynchronizationHandle IDistributedLock.Acquire(TimeSpan? timeout, CancellationToken cancellationToken)
-    {
-        return Acquire(timeout, cancellationToken);
-    }
-
-    ValueTask<IDistributedSynchronizationHandle?> IDistributedLock.TryAcquireAsync(TimeSpan timeout, CancellationToken cancellationToken)
-    {
-        return TryAcquireAsync(timeout, cancellationToken).Convert(To<IDistributedSynchronizationHandle?>.ValueTask);
-    }
-
-    ValueTask<IDistributedSynchronizationHandle> IDistributedLock.AcquireAsync(TimeSpan? timeout, CancellationToken cancellationToken)
-    {
-        return AcquireAsync(timeout, cancellationToken).Convert(To<IDistributedSynchronizationHandle>.ValueTask);
-    }
+    IDistributedSynchronizationHandle? IDistributedLock.TryAcquire(TimeSpan timeout, CancellationToken cancellationToken) =>
+        this.TryAcquire(timeout, cancellationToken);
+    IDistributedSynchronizationHandle IDistributedLock.Acquire(TimeSpan? timeout, CancellationToken cancellationToken) =>
+        this.Acquire(timeout, cancellationToken);
+    ValueTask<IDistributedSynchronizationHandle?> IDistributedLock.TryAcquireAsync(TimeSpan timeout, CancellationToken cancellationToken) =>
+        this.TryAcquireAsync(timeout, cancellationToken).Convert(To<IDistributedSynchronizationHandle?>.ValueTask);
+    ValueTask<IDistributedSynchronizationHandle> IDistributedLock.AcquireAsync(TimeSpan? timeout, CancellationToken cancellationToken) =>
+        this.AcquireAsync(timeout, cancellationToken).Convert(To<IDistributedSynchronizationHandle>.ValueTask);
 
     /// <summary>
-    /// Attempts to acquire the lock synchronously. Usage:
+    /// Attempts to acquire the lock synchronously. Usage: 
     /// <code>
     ///     using (var handle = myLock.TryAcquire(...))
     ///     {
@@ -38,14 +27,12 @@ public partial class MongoDistributedLock
     /// </summary>
     /// <param name="timeout">How long to wait before giving up on the acquisition attempt. Defaults to 0</param>
     /// <param name="cancellationToken">Specifies a token by which the wait can be canceled</param>
-    /// <returns>A <see cref="MongoDistributedLockHandle" /> which can be used to release the lock or null on failure</returns>
-    public MongoDistributedLockHandle? TryAcquire(TimeSpan timeout = default, CancellationToken cancellationToken = default)
-    {
-        return DistributedLockHelpers.TryAcquire(this, timeout, cancellationToken);
-    }
+    /// <returns>A <see cref="MongoDistributedLockHandle"/> which can be used to release the lock or null on failure</returns>
+    public MongoDistributedLockHandle? TryAcquire(TimeSpan timeout = default, CancellationToken cancellationToken = default) =>
+        DistributedLockHelpers.TryAcquire(this, timeout, cancellationToken);
 
     /// <summary>
-    /// Acquires the lock synchronously, failing with <see cref="TimeoutException" /> if the attempt times out. Usage:
+    /// Acquires the lock synchronously, failing with <see cref="TimeoutException"/> if the attempt times out. Usage: 
     /// <code>
     ///     using (myLock.Acquire(...))
     ///     {
@@ -54,16 +41,14 @@ public partial class MongoDistributedLock
     ///     // dispose releases the lock
     /// </code>
     /// </summary>
-    /// <param name="timeout">How long to wait before giving up on the acquisition attempt. Defaults to <see cref="Timeout.InfiniteTimeSpan" /></param>
+    /// <param name="timeout">How long to wait before giving up on the acquisition attempt. Defaults to <see cref="Timeout.InfiniteTimeSpan"/></param>
     /// <param name="cancellationToken">Specifies a token by which the wait can be canceled</param>
-    /// <returns>A <see cref="MongoDistributedLockHandle" /> which can be used to release the lock</returns>
-    public MongoDistributedLockHandle Acquire(TimeSpan? timeout = null, CancellationToken cancellationToken = default)
-    {
-        return DistributedLockHelpers.Acquire(this, timeout, cancellationToken);
-    }
+    /// <returns>A <see cref="MongoDistributedLockHandle"/> which can be used to release the lock</returns>
+    public MongoDistributedLockHandle Acquire(TimeSpan? timeout = null, CancellationToken cancellationToken = default) =>
+        DistributedLockHelpers.Acquire(this, timeout, cancellationToken);
 
     /// <summary>
-    /// Attempts to acquire the lock asynchronously. Usage:
+    /// Attempts to acquire the lock asynchronously. Usage: 
     /// <code>
     ///     await using (var handle = await myLock.TryAcquireAsync(...))
     ///     {
@@ -74,14 +59,12 @@ public partial class MongoDistributedLock
     /// </summary>
     /// <param name="timeout">How long to wait before giving up on the acquisition attempt. Defaults to 0</param>
     /// <param name="cancellationToken">Specifies a token by which the wait can be canceled</param>
-    /// <returns>A <see cref="MongoDistributedLockHandle" /> which can be used to release the lock or null on failure</returns>
-    public ValueTask<MongoDistributedLockHandle?> TryAcquireAsync(TimeSpan timeout = default, CancellationToken cancellationToken = default)
-    {
-        return this.As<IInternalDistributedLock<MongoDistributedLockHandle>>().InternalTryAcquireAsync(timeout, cancellationToken);
-    }
+    /// <returns>A <see cref="MongoDistributedLockHandle"/> which can be used to release the lock or null on failure</returns>
+    public ValueTask<MongoDistributedLockHandle?> TryAcquireAsync(TimeSpan timeout = default, CancellationToken cancellationToken = default) =>
+        this.As<IInternalDistributedLock<MongoDistributedLockHandle>>().InternalTryAcquireAsync(timeout, cancellationToken);
 
     /// <summary>
-    /// Acquires the lock asynchronously, failing with <see cref="TimeoutException" /> if the attempt times out. Usage:
+    /// Acquires the lock asynchronously, failing with <see cref="TimeoutException"/> if the attempt times out. Usage: 
     /// <code>
     ///     await using (await myLock.AcquireAsync(...))
     ///     {
@@ -90,11 +73,9 @@ public partial class MongoDistributedLock
     ///     // dispose releases the lock
     /// </code>
     /// </summary>
-    /// <param name="timeout">How long to wait before giving up on the acquisition attempt. Defaults to <see cref="Timeout.InfiniteTimeSpan" /></param>
+    /// <param name="timeout">How long to wait before giving up on the acquisition attempt. Defaults to <see cref="Timeout.InfiniteTimeSpan"/></param>
     /// <param name="cancellationToken">Specifies a token by which the wait can be canceled</param>
-    /// <returns>A <see cref="MongoDistributedLockHandle" /> which can be used to release the lock</returns>
-    public ValueTask<MongoDistributedLockHandle> AcquireAsync(TimeSpan? timeout = null, CancellationToken cancellationToken = default)
-    {
-        return DistributedLockHelpers.AcquireAsync(this, timeout, cancellationToken);
-    }
+    /// <returns>A <see cref="MongoDistributedLockHandle"/> which can be used to release the lock</returns>
+    public ValueTask<MongoDistributedLockHandle> AcquireAsync(TimeSpan? timeout = null, CancellationToken cancellationToken = default) =>
+        DistributedLockHelpers.AcquireAsync(this, timeout, cancellationToken);
 }
