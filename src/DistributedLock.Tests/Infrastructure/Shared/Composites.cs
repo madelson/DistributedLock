1a1f5d265e885a3a9774e04544dca29a9446abd6
madelson <1269046+madelson@users.noreply.github.com>
2026-01-17 22:51:18 -0500

    Fix build

--- src/DistributedLock.Tests/Infrastructure/Shared/Composites.cs
+++ src/DistributedLock.Tests/Infrastructure/Shared/Composites.cs
@@ -51,7 +51,7 @@ public class TestingCompositeWaitHandleDistributedSemaphore(string name, int max
         this._provider.TryAcquireAllSemaphoresAsync(this._names, maxCount, timeout, cancellationToken);
 }
 
-public class TestingCompositePostgresReaderWriterLock(string name, string connectionString, Action<PostgresConnectionOptionsBuilder>? options) : IDistributedReaderWriterLock
+public class TestingCompositePostgresReaderWriterLock(string name, string connectionString, Action<PostgresConnectionOptionsBuilder>? options = null) : IDistributedReaderWriterLock
 {
     private readonly PostgresDistributedSynchronizationProvider _provider = new(connectionString, options);
     private readonly string[] _names = [name + "_1", name + "_2"];
